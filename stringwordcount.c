1fbe52694be60f6bc05fdc92aac3f3eaaadcb616
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2019-04-18 04:07:25 +0530

    fixed the code
    had errors and was not working at all.

--- stringwordcount.c
+++ stringwordcount.c
@@ -3,13 +3,13 @@ void main()
 {
     char ch[20];
     int i,word;
-    printf("\n enter the string");
+    printf("enter the string \n");
     gets(ch);
     i=0;
     word=1;
-    while(ch[i]='\0')
+    while(ch[i] != '\0')
     {
-        if(ch[i]==''||ch[i]=='\n'||ch[i]=='\t')
+        if(ch[i]==' '|| ch[i]=='\n'|| ch[i]=='\t')
         {
           word++;
         }
